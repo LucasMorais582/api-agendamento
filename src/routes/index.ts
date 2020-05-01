2ad1567e5361613c9b0ba8253bface60d4272d41
Lucas <lucasmorais582@gmail.com>
2020-05-01 18:37:46 -0300

    Criação dos serviços GET e POST de agendamento

---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import { Router, json } from 'express';
+import appointmentsRouter from './appointments.routes';
+
+const routes = Router();
+routes.use(json());
+routes.use('/appointments', appointmentsRouter);
+
+export default routes;
