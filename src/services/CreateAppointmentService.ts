f07ef08db4f41973446fd7d73ba73f4f8cbadb0d
Lucas <lucasmorais582@gmail.com>
2020-05-19 02:15:11 -0300

    Integração com banco de dados e com TypeORM

--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,4 +1,5 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointments';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
@@ -8,25 +9,22 @@ interface Request {
 }
 
 class CreateAppointmentService {
-  private appointmentsRepository: AppointmentsRepository;
-
-  constructor(appointmentsRepository: AppointmentsRepository) {
-    this.appointmentsRepository = appointmentsRepository;
-  }
-
-  public execute({ provider, date }: Request): Appointment {
+  public async execute({ provider, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
     const appointmentDate = startOfHour(date);
-    const findAppointmentIsSameDate = this.appointmentsRepository.findByDate(
-      date
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+      appointmentDate
     );
 
-    if (findAppointmentIsSameDate)
+    if (findAppointmentInSameDate)
       throw Error('This appointment is already booked');
 
-    const appointment = this.appointmentsRepository.create({
+    const appointment = appointmentsRepository.create({
       provider,
       date: appointmentDate,
     });
+
+    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
