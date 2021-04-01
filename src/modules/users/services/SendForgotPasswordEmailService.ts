f85c7b8753aa31ee7fdd8cae39b25aceddcbab19
Lucas <lucasmorais582@gmail.com>
2021-04-01 18:34:22 -0300

    Criação de testes de recuperação de senha

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,38 @@
+import { inject, injectable } from 'tsyringe';
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '@shared/errors/AppError';
+import IUserRepository from '../repositories/IUserRepository';
+import IUserTokensRepository from '../repositories/IUserTokenRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private userRepository: IUserRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokensRepository
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) throw new AppError('User does not exist.');
+
+    await this.userTokensRepository.generate(user.id);
+
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido.'
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
